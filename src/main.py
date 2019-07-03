97941ad3892e1739a065b1414b1d96e9a766fdbf
Hinrikus Wolf <hinrikus@cs.rwth-aachen.de>
2019-07-03 11:54:33 +0200

    fix deprication

--- src/main.py
+++ src/main.py
@@ -89,7 +89,7 @@ def learn_embeddings(walks):
     walks = [str(walk) for walk in walks]
     model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers,
                      iter=args.iter)
-    model.save_word2vec_format(args.output)
+    model.wv.save_word2vec_format(args.output)
 
     return
 
